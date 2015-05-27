1aaef999ef462ec69ec0f0798a75593ab5dc4ebe
kylec12 <kcalloway@atl.yearup.org>
2015-05-26 21:28:48 -0400

    main one

---
+++ Encapsulation1/src/main1.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) 
+	{
+		GrowsOlder firstGrowth = new age();
+		
+		System.out.println(firstGrowth.GetAge() + "to" + firstGrowth.GetName());
+
+	}
+
+}
